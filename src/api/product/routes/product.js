f9284624abc272a9ece19e640401f7d832d45e58
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2022-09-14 20:08:41 +0700

    Update Structure

--- src/api/product/routes/product.js
+++ src/api/product/routes/product.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * article router
+ * product router
  */
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::article.article');
+module.exports = createCoreRouter('api::product.product');
--- src/api/product/services/product.js
+++ src/api/product/services/product.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * article service
+ * product service
  */
 
 const { createCoreService } = require('@strapi/strapi').factories;
 
-module.exports = createCoreService('api::article.article');
+module.exports = createCoreService('api::product.product');
